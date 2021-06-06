dedba15b95436265147482b7c23546fb7ece78b4
Куулар Бурбу Омакович <kuular.burbu@otr.ru>
2021-06-06 11:20:08 +0300

    bugfix/change function name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
-window.renderWallet = (containerId, history) => {
+window.renderDeposit = (containerId, history) => {
   ReactDOM.render(
     <App history={history} />,
     document.getElementById(containerId),
@@ -13,7 +13,7 @@ window.renderWallet = (containerId, history) => {
   serviceWorker.unregister();
 };
 
-window.unmountWallet = containerId => {
+window.unmountDeposit = containerId => {
   ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
 };
 
