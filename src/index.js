4f1fcd8f8b18488469d1217644c9424d8df86325
SAMAKOV ULUKBEK <uluk.samakov@gmail.com>
2021-06-06 12:37:19 +0600

    conf

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+window.renderWallet = (containerId, history) => {
+  ReactDOM.render(
+    <App history={history} />,
+    document.getElementById(containerId),
+  );
+  serviceWorker.unregister();
+};
+
+window.unmountWallet = containerId => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+if (!document.getElementById('Deposit-container')) {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  serviceWorker.unregister();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
